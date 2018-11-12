9449eb614381d98b7c78b59a41022b60cbd21bd0
salaz <salaz@DESKTOP-IVOPF5P>
2018-11-12 19:59:07 +0100

    set y get en posicion

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -4,4 +4,35 @@ public class Posicion {
 	private int fila;
 	private char columna;
 
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila<1 || fila>8) {
+			throw new IllegalArgumentException("El valor de la fila no es correcto.");
+		} else {
+			this.fila = fila;
+		}
+
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if (columna == 'a' || columna == 'b' || columna == 'c' || columna == 'd' || columna == 'e' || columna == 'f' || 
+				columna == 'g' || columna =='h') {
+			this.columna = columna;
+		} else {
+			throw new IllegalArgumentException("La posición de la columna no es correcta.");
+		}
+
+
+
+
+	}
+
+
+
+
+
 }
