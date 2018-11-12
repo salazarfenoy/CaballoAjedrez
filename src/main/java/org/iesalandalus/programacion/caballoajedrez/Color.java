109671bf2a4fe969ded26f544a92b928f0d1c440
salaz <salaz@DESKTOP-IVOPF5P>
2018-11-12 19:02:36 +0100

    Enum Direccion

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Color.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.caballoajedrez;
+
+public enum Color {
+	BLANCO, NEGRO;
+	
+
+}
---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Direccion.java
@@ -0,0 +1,6 @@
+package org.iesalandalus.programacion.caballoajedrez;
+
+public enum Direccion {
+	ARRIBA_IZQUIERDA, ARRIBA_DERECHA, DERECHA_ARRIBA, DERECHA_ABAJO, ABAJO_DERECHA, ABAJO_IZQUIERDA, IZQUIERDA_ARRIBA, IZQUIERDA_ABAJO;
+
+}
