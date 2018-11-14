ac04f5cd1ef7e4624a1ee048678958acc749de28
salaz <salaz@DESKTOP-IVOPF5P>
2018-11-14 12:16:09 +0100

    14. Caballo blanco o negro

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -8,18 +8,28 @@ public class Caballo {
 		color = Color.NEGRO;
 		posicion = new Posicion(8, 'b');
 	}
-	
-	public Color getColor() {
-		return color;
+
+	public Caballo (String color) {
+		if (this.color == Color.NEGRO){
+			posicion = new Posicion(8, 'b');
+		}
+		if (this.color == Color.BLANCO) {
+			posicion = new Posicion(1, 'b');
 	}
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	public Posicion getPosicion() {
-		return posicion;
-	}
-	public void setPosicion(Posicion posicion) {
-		this.posicion = posicion;
-	}
+
+}
+
+public Color getColor() {
+	return color;
+}
+public void setColor(Color color) {
+	this.color = color;
+}
+public Posicion getPosicion() {
+	return posicion;
+}
+public void setPosicion(Posicion posicion) {
+	this.posicion = posicion;
+}
 
 }
