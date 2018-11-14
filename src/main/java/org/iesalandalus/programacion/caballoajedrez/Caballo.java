f832c53fcc4a6dd199a0b76cbef4255dea923a4f
salaz <salaz@DESKTOP-IVOPF5P>
2018-11-14 12:01:44 +0100

    12. Métodos get y set para Caballo

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -3,5 +3,18 @@ package org.iesalandalus.programacion.caballoajedrez;
 public class Caballo {
 	private Color color;
 	private Posicion posicion;
+	
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
 
 }
