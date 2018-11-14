4dcd020e0802e56243ccea50d0714fe5106beeb8
salaz <salaz@DESKTOP-IVOPF5P>
2018-11-14 12:09:57 +0100

    13. Caballo negro en 8b

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -4,6 +4,11 @@ public class Caballo {
 	private Color color;
 	private Posicion posicion;
 	
+	public Caballo() {
+		color = Color.NEGRO;
+		posicion = new Posicion(8, 'b');
+	}
+	
 	public Color getColor() {
 		return color;
 	}
